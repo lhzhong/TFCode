7077f0a13f31dd3e2a2920150ca358076dbf00f7
lhzhong <zhong_luhao@163.com>
2018-04-04 22:04:55 +0800

    Add files via upload

---
+++ CIFAR10/finetune/evaluate.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr  3 18:58:32 2018
+
+@author: zhong
+"""
+
+import math
+import numpy as np
+import tensorflow as tf
+
+import input_data
+import VGG
+import tools
+
+IMG_W = 32
+IMG_H = 32
+N_CLASSES = 10
+BATCH_SIZE = 32
+learning_rate = 0.01
+IS_PRETRAIN = True
+
+def evaluate_running():
+    with tf.Graph().as_default():
+        
+        model_dir = './model/'
+        test_dir = './data/'
+        n_test = 10000
+                
+        images, labels = input_data.read_cifar10(data_dir=test_dir,
+                                                    is_train=False,
+                                                    batch_size= BATCH_SIZE,
+                                                    shuffle=False)
+
+        logits = VGG.VGG16N(images, N_CLASSES, IS_PRETRAIN)
+        correct = tools.num_correct_prediction(logits, labels)
+        saver = tf.train.Saver(tf.global_variables())
+        
+        with tf.Session() as sess:
+            
+            print("Reading checkpoints...")
+            ckpt = tf.train.get_checkpoint_state(model_dir)
+            if ckpt and ckpt.model_checkpoint_path:
+                global_step = ckpt.model_checkpoint_path.split('/')[-1].split('-')[-1]
+                saver.restore(sess, ckpt.model_checkpoint_path)
+                print('Loading success, global_step is %s' % global_step)
+            else:
+                print('No checkpoint file found')
+                return
+        
+            coord = tf.train.Coordinator()
+            threads = tf.train.start_queue_runners(sess = sess, coord = coord)
+            
+            try:
+                print('\nEvaluating......')
+                num_step = int(math.floor(n_test / BATCH_SIZE))
+                num_sample = num_step*BATCH_SIZE
+                step = 0
+                total_correct = 0
+                while step < num_step and not coord.should_stop():
+                    batch_correct = sess.run(correct)
+                    total_correct += np.sum(batch_correct)
+                    step += 1
+                print('Total testing samples: %d' %num_sample)
+                print('Total correct predictions: %d' %total_correct)
+                print('Average accuracy: %.2f%%' %(100*total_correct/num_sample))
+            except Exception as e:
+                coord.request_stop(e)
+            finally:
+                coord.request_stop()
+                coord.join(threads)
+                
+if __name__ == '__main__':
+    evaluate_running()
