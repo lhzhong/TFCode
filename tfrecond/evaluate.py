604fb5d0e7c25ab2a8625e09fd149b22cf090b0b
lhzhong <lhzhong5110@gmail.com>
2018-04-17 22:23:18 +0800

    Add files via upload

---
+++ tfrecond/evaluate.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 31 15:34:18 2018
+
+@author: zhong
+"""
+
+import numpy as np
+import tensorflow as tf
+import input_data
+import models
+import math
+
+N_CLASSES = 6                                    #### notice
+IMG_W = 80  
+IMG_H = 60
+RATIO = 0.3
+BATCH_SIZE = 128
+MIN_AFTER_DEQUENE = 512
+
+data_dir = './data/KTH_RGB/'
+model_dir = './model/KTH_RGB100000_AlexNet_bn/'          #### notice
+val_tfrecords_file ='val.tfrecords'
+
+def evaluate_running():
+  
+    with tf.Graph().as_default():
+        
+        val_batch, val_label_batch = input_data.read_and_decode(val_tfrecords_file, IMG_W, IMG_H, BATCH_SIZE, MIN_AFTER_DEQUENE)
+
+        _,_,_,_,n_test = input_data.get_files(data_dir, RATIO, ret_val_num=True)
+        
+        logits = models.AlexNet(val_batch, N_CLASSES)    #### notice
+        top_k_op = tf.nn.in_top_k(logits, val_label_batch, 1)
+    
+        saver = tf.train.Saver(tf.global_variables())
+        
+        with tf.Session() as sess:
+            
+            print("Reading checkpoints...")
+            ckpt = tf.train.get_checkpoint_state(model_dir)
+            if ckpt and ckpt.model_checkpoint_path:
+                global_step = ckpt.model_checkpoint_path.split('/')[-1].split('-')[-1]
+                saver.restore(sess, ckpt.model_checkpoint_path)
+                print('Loading success, global_step is %s' % global_step)
+            else:
+                print('No checkpoint file found')
+                return
+        
+            coord = tf.train.Coordinator()
+            threads = tf.train.start_queue_runners(sess = sess, coord = coord)
+            
+            try:
+                
+                num_iter = int(math.ceil(n_test / BATCH_SIZE))
+                true_count = 0
+                total_sample_count = num_iter * BATCH_SIZE
+                step = 0
+    
+                while step < num_iter and not coord.should_stop():
+                    val_images_,val_labels_ = sess.run([val_batch, val_label_batch])
+                    predictions = sess.run([top_k_op])
+                    true_count += np.sum(predictions)
+                    step += 1
+                precision = true_count / total_sample_count*100.0
+                print('precision = %.2f%%' % precision)
+            except Exception as e:
+                coord.request_stop(e)
+            finally:
+                coord.request_stop()
+            coord.join(threads)
+
+if __name__ == '__main__':
+    evaluate_running()
---
+++ tfrecond/input_data.py
@@ -0,0 +1,165 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Mar 29 08:03:17 2018
+
+@author: zhong
+"""
+
+#%%
+
+import tensorflow as tf
+import numpy as np
+import os
+import math
+import cv2
+from datetime import datetime
+
+#%%
+
+def get_files(file_dir, ratio, ret_val_num=False):
+    '''
+    Args:
+        file_dir: file directory
+        ratio: the ration of val samples
+    Returns:
+        list of images and labels
+    '''
+    class_train = []
+    label_train = []
+    k=0
+    for train_class in os.listdir(file_dir):
+        for sub_train in os.listdir(file_dir+train_class):
+            for image in os.listdir(file_dir+train_class+'/'+sub_train) :
+                class_train.append(file_dir+train_class+'/'+sub_train+'/'+image)
+                label_train.append(k)
+        k=k+1
+    temp = np.array([class_train,label_train])
+    temp = temp.transpose()
+    #shuffle the samples
+    np.random.shuffle(temp)
+    #after transpose, images is in dimension 0 and label in dimension 1
+    all_image_list = list(temp[:,0])
+    all_label_list = list(temp[:,1])
+    n_sample = len(all_label_list)
+    n_val = int(math.ceil(n_sample*ratio)) #测试样本数
+    n_train = n_sample - n_val # 训练样本数
+    tra_images = all_image_list[0:n_train]
+    tra_labels = all_label_list[0:n_train]
+    tra_labels = [int(float(i)) for i in tra_labels]
+    val_images = all_image_list[n_train:-1]
+    val_labels = all_label_list[n_train:-1]
+    val_labels = [int(float(i)) for i in val_labels]
+    print("Training picture: %d, %d classes" %(n_train,k))
+    print("Testing picture: %d, %d classes" %(n_val,k))
+    if ret_val_num:
+        return tra_images,tra_labels,val_images,val_labels,n_val
+    else:
+        return tra_images,tra_labels,val_images,val_labels
+
+#%%
+def int64_feature(value):
+  """Wrapper for inserting int64 features into Example proto."""
+  if not isinstance(value, list):
+    value = [value]
+  return tf.train.Feature(int64_list=tf.train.Int64List(value=value))
+
+def bytes_feature(value):
+  return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
+
+#%%
+
+def convert_to_tfrecord(images, labels, name):
+    '''convert all images and labels to one tfrecord file.
+    Args:
+        images: list of image directories, string type
+        labels: list of labels, int type
+        save_dir: the directory to save tfrecord file, e.g.: '/home/folder1/'
+        name: the name of tfrecord file, string type, e.g.: 'train'
+    Return:
+        no return
+    Note:
+        converting needs some time, be patient...
+    '''
+    start_time = datetime.now()
+    filename = os.path.join(name + '.tfrecords')
+    n_samples = len(labels)
+    
+    if np.shape(images)[0] != n_samples:
+        raise ValueError('Images size %d does not match label size %d.' %(images.shape[0], n_samples))
+    
+    # wait some time here, transforming need some time based on the size of your data.
+    writer = tf.python_io.TFRecordWriter(filename)
+    print('\nTransform %s data......'%name)
+    for i in np.arange(0, n_samples):
+        try:
+            image = cv2.imread(images[i]) 
+            image = cv2.resize(image, (160,120)) ###
+            image_raw = image.tostring()
+            label = int(labels[i])
+            example = tf.train.Example(features=tf.train.Features(feature={
+                            'label':int64_feature(label),
+                            'image_raw': bytes_feature(image_raw)}))
+            writer.write(example.SerializeToString())
+        except IOError as e:
+            print('Could not read:', images[i])
+            print('error: %s' %e)
+            print('Skip it!\n')
+    writer.close()
+    end_time = datetime.now()
+    print('Transform %s data done!'%name)
+    print((end_time-start_time))
+    
+    
+#%%    
+def generate_tfrecond(data_dir, ratio):
+
+    name_train = 'train'
+    name_val = 'val'
+    tra_images,tra_labels,val_images,val_labels = get_files(data_dir, ratio)
+    convert_to_tfrecord(tra_images, tra_labels, name_train)
+    convert_to_tfrecord(val_images, val_labels, name_val)
+
+#%%  
+def read_and_decode(tfrecords_file, image_W, image_H, batch_size, min_after_dequeue):
+    '''read and decode tfrecord file, generate (image, label) batches
+    Args:
+        tfrecords_file: the directory of tfrecord file
+        image_W: image width
+        image_H: image height
+        batch_size: number of images in each batch
+        capacity: the maximum elements in queue
+    Returns:
+        image: 4D tensor - [batch_size, width, height, channel]
+        label: 1D tensor - [batch_size]
+    '''
+    # make an input queue from the tfrecord file
+    filename_queue = tf.train.string_input_producer([tfrecords_file])
+    
+    reader = tf.TFRecordReader()
+    _, serialized_example = reader.read(filename_queue)
+    img_features = tf.parse_single_example(
+                                        serialized_example,
+                                        features={
+                                               'label': tf.FixedLenFeature([], tf.int64),
+                                               'image_raw': tf.FixedLenFeature([], tf.string),
+                                               })
+    image = tf.decode_raw(img_features['image_raw'], tf.uint8)
+    image = tf.reshape(image, [160,120,3]) ####
+    label = tf.cast(img_features['label'], tf.int32)  
+    
+    ##########################################################
+    # you can put data augmentation here, I didn't use it
+    ##########################################################
+    
+    image = tf.image.resize_image_with_crop_or_pad(image, image_W, image_H) ###
+    image = tf.image.per_image_standardization(image)
+    capacity = 2*min_after_dequeue
+    image_batch, label_batch = tf.train.shuffle_batch([image, label],
+                                                batch_size= batch_size,
+                                                min_after_dequeue= min_after_dequeue, 
+                                                capacity = capacity)
+    label_batch = tf.reshape(label_batch, [batch_size])
+    image_batch = tf.cast(image_batch, tf.float32)
+    
+    return image_batch, label_batch
